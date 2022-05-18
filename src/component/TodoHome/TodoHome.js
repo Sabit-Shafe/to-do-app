f80d258099d00162c1e8bd522a89ec6d5d7ef294
Sabitshafeexit(0clear <sabitshaafee@gmail.com>
2022-05-18 21:02:26 +0600

    first commit

---
+++ src/component/TodoHome/TodoHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoHome = () => {
+    return (
+        <div>
+            <h1 className="text-primary">hello</h1>
+        </div>
+    );
+};
+
+export default TodoHome;
